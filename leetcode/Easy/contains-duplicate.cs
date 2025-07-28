37c115a20145be1833a6a08b5b639f0fa9d58aad
Praveen Kumar <kpraveen@ariqt.com>
2025-07-28 19:52:44 +0530

    Add project files.

---
+++ leetcode/Easy/contains-duplicate.cs
@@ -0,0 +1,17 @@
+﻿
+namespace leetcode.Easy
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seen = new();
+            foreach (int num in nums)
+            {
+                if (seen.Contains(num)) return true;
+                seen.Add(num);
+            }
+            return false;
+        }
+    }
+}
