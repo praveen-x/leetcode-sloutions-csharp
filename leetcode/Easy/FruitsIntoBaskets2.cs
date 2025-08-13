6482d5e475fbb396b2a7438fbdf1eb61574e50a8
Praveen <praveeen.work@gmail.com>
2025-08-14 00:15:58 +0530

    ---

--- leetcode/Easy/FruitsIntoBaskets2.cs
+++ leetcode/Easy/FruitsIntoBaskets2.cs
@@ -2,9 +2,10 @@ public class FruitsIntoBaskets2Solution {
     public int NumOfUnplacedFruits(int[] fruits, int[] baskets) {
        
         int result = fruits.Length;
+        int n = fruits.Length;
         bool[] used = new bool[baskets.Length];
 
-        for (int i = 0; i < fruits.Length; i++)
+        for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < baskets.Length; j++)
             {
